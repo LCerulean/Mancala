11d6a34ced696984181e4120584c7a8ee8221fa5
ObsessivelyBlue <118937230+ObsessivelyBlue@users.noreply.github.com>
2023-05-17 14:14:46 -0400

    Changed all instances of 'seeds' to 'stones'.

--- mancala.py
+++ mancala.py
@@ -21,19 +21,19 @@ def start_message():
     display() 
 
 
-#Game Rules Message
+#game rules
 def rules():
     display()
     print("RULES:\n")
-    print("The board consists of 12 pits, each with 4 seeds, and 2 empty bowls.\nYour pits are the 6 on the bottom and your bowl is above your name.")
+    print("The board consists of 12 pits, each with 4 stones, and 2 empty bowls.\nYour pits are the 6 on the bottom and your bowl is above your name.")
     time.sleep(5)
-    print("\nOn your turn you will pick one of your 6 pits.\nThe seeds will be redistributed, 1 per pit, counter-clockwise.\nIf you pass your bowl, a seed goes in there too.")
+    print("\nOn your turn you will pick one of your 6 pits.\nThe stones will be redistributed, 1 per pit, counter-clockwise.\nIf you pass your bowl, a stone goes in there too.")
     time.sleep(5)
-    print("\nIf your last seed lands in your bowl, you get a free turn!\nYou might be able to do this multiple times in a row if you plan well.")
+    print("\nIf your last stone lands in your bowl, you get a free turn!\nYou might be able to do this multiple times in a row if you plan well.")
     time.sleep(5)
-    print("\nIf your last seed lands in one of your empty pits, you take the opposite pit’s seeds!\nThis only works if your opponent has seeds in the opposite pit.\nThose seeds are then auto-redistributed around the board like a regular turn.")
+    print("\nIf your last stone lands in one of your empty pits, you take the opposite pit’s stones!\nThis only works if your opponent has stones in the opposite pit.\nThose stones are then auto-redistributed around the board like a regular turn.")
     time.sleep(5)
-    print("\nThe game ends when one side does not have any seeds left in their 6 pits.\nThe final score counts both your bowl and any seeds you have left in your pits.\nBe careful not to run out of seeds in your pits if your opponent still has a lot in theirs!")
+    print("\nThe game ends when one side does not have any stones left in their 6 pits.\nThe final score counts both your bowl and any stones you have left in your pits.\nBe careful not to run out of stones in your pits if your opponent still has a lot in theirs!")
     time.sleep(6)
     print("\nThat's it, have fun!")
 
@@ -65,13 +65,13 @@ def display():
     print(f"{header}\n{comp_pits}\n{bowls}\n{player_pits}\n{footer}")
 
 
-#gets the pit selection from player and the seed count of said pit
-def select_pit_take_seeds(who_has_turn):
-    seeds = 0
+#gets the pit selection from player and the stone count of said pit
+def select_pit_take_stones(who_has_turn):
+    stones = 0
     if who_has_turn == player:
-        while seeds == 0:
+        while stones == 0:
             pit = input("\nPick a pit: ")
-            #gets seed count or gives error message if out of range of player pits
+            #gets stone count or gives error message if out of range of player pits
             try:
                 pit = int(pit)
                 if pit > 6 or pit < 1:
@@ -79,7 +79,7 @@ def select_pit_take_seeds(who_has_turn):
                 elif pits[pit] == 0:
                     print("Sorry, that pit is empty")
                 else:
-                    seeds = pits[pit]
+                    stones = pits[pit]
             #error message if input is non-numeric, unless input is 'rules', then shows rules
             except:
                 if pit.lower() == "rules":
@@ -89,7 +89,7 @@ def select_pit_take_seeds(who_has_turn):
                     print("That's not an option, please pick a number between 1 and 6.")
     else:
         print(f"\n{computer} is picking a pit...")
-        while seeds == 0:
+        while stones == 0:
             #looking for free move, else picking random
             for i in range(8,14):
                 num_to_hit_bowl = i - 7
@@ -98,41 +98,41 @@ def select_pit_take_seeds(who_has_turn):
                     break
                 else:
                     pit = random.randint(8,13)
-            seeds = pits[pit]
+            stones = pits[pit]
     
     time.sleep(1)
     if who_has_turn == player:
-        print(f"\n{who_has_turn} takes {seeds} seeds from pit {pit}.")
+        print(f"\n{who_has_turn} takes {stones} stones from pit {pit}.")
     else:
-        print(f"\n{who_has_turn} takes {seeds} seeds from pit {str(int(pit - 7))}.")
+        print(f"\n{who_has_turn} takes {stones} stones from pit {str(int(pit - 7))}.")
     time.sleep(1)
-    print("\nRedistributing seeds and updating gameboard...")
+    print("\nRedistributing stones and updating gameboard...")
     time.sleep(1)
-    return pit, seeds
+    return pit, stones
 
 
 #takes pit chosen by player/computer and updates pits dict after redistributing
-def redistribute_seeds(who_has_turn, pit, seeds):
+def redistribute_stones(who_has_turn, pit, stones):
     pits.update({pit:0})
     if who_has_turn == computer:
         not_my_bowl = 0
     else:
         not_my_bowl = 7
 
-    #distributes the seeds counter-clockwise, adding to owner's bowl if passed
-    for seed in range(seeds):
+    #distributes the stones counter-clockwise, adding to owner's bowl if passed
+    for stone in range(stones):
         if pit - 1 < 0 or pit - 1 == not_my_bowl:
             pit = 13
-            seeds_in_pit = pits[pit]
-            seeds_in_pit += 1
-            pits.update({pit:seeds_in_pit})
+            stones_in_pit = pits[pit]
+            stones_in_pit += 1
+            pits.update({pit:stones_in_pit})
         else:
             pit -= 1
-            seeds_in_pit = pits[pit]
-            seeds_in_pit += 1
-            pits.update({pit:seeds_in_pit})
+            stones_in_pit = pits[pit]
+            stones_in_pit += 1
+            pits.update({pit:stones_in_pit})
     
-    #last pit a seed was put in
+    #last pit a stone was put in
     return pit
 
 
@@ -167,25 +167,25 @@ def play_game():
             pits_owned = list(range(1,7))
             opponent = computer
 
-        #makes move and seeds are redistributed
+        #makes move and stones are redistributed
         while pit == bowl and game_over == False:
-            move = select_pit_take_seeds(who_has_turn)
+            move = select_pit_take_stones(who_has_turn)
             pit = move[0]
-            seeds = move[1]
-            pit = redistribute_seeds(who_has_turn, pit, seeds)
+            stones = move[1]
+            pit = redistribute_stones(who_has_turn, pit, stones)
             display() 
 
-            #checks if last landing pit qualifies for taking oppenent's seeds
+            #checks if last landing pit qualifies for taking oppenent's stones
             if pit in pits_owned and pits[pit] == 1 and pits[op_pits[pit]] > 0:
                 print(f"\n{who_has_turn} takes {pits[op_pits[pit]]} from {opponent}'s pit!")
                 time.sleep(1)
-                print("\nRedistributing seeds and updating gameboard...")
+                print("\nRedistributing stones and updating gameboard...")
                 time.sleep(1)
                 while pit in pits_owned and pits[pit] == 1 and pits[op_pits[pit]] > 0 and game_over == False:
                     pits[pit] += pits[op_pits[pit]] 
                     pits[op_pits[pit]] = 0
-                    seeds = pits[pit]
-                    pit = redistribute_seeds(player, pit, seeds)
+                    stones = pits[pit]
+                    pit = redistribute_stones(player, pit, stones)
                     
                     #prevents another auto move if game over after last one
                     game_over = game_status(pits)
@@ -195,7 +195,7 @@ def play_game():
             #prevents being stuck in loop if game over before turn switch
             game_over = game_status(pits)  
 
-            #free move message if last seed landed in the bowl
+            #free move message if last stone landed in the bowl
             if pit == 7 and game_over == False:
                 print("\nFree move!")
                 time.sleep(1)
@@ -222,7 +222,7 @@ def end_game():
     for i in range(1,7):
         player_pits_total += pits[i]
         pits[i] = 0
-    #adds remaining seeds from pits to bowls
+    #adds remaining stones from pits to bowls
     pits[0] += player_pits_total
     pits[7] += comp_pits_total
     player_score = pits[0]
@@ -243,10 +243,10 @@ def end_game():
         print(f"The winner is {winner}!\n")
 
 
-#the seed distribution of the board
+#the stone distribution of the board
 pits = {0:0, 1:4, 2:4, 3:4, 4:4, 5:4, 6:4, 7:0, 8:4, 9:4, 10:4, 11:4, 12:4, 13:4}
 op_pits = {1:13, 2:12, 3:11, 4:10, 5:9, 6:8, 8:6, 9:5, 10:4, 11:3, 12:2, 13:1}
-# seeds = 0
+# stones = 0
 # pit = 0
 
 #names for the players, can be changed to whatever
